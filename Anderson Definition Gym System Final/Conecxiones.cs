c9c57cdee8e4de04dccbea0113e23a9ec4b65378
danny de leon <danydeleon26@gmail.com>
2018-05-06 15:03:12 -0400

    busqueda inteligente primer commit

---
+++ Anderson Definition Gym System Final/Conecxiones.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Anderson_Definition_Gym_System_Final
+{
+    class Conecxiones
+    {
+
+        SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=andersonGym;Integrated Security=True");
+
+
+        #region Conectar con la base de datos
+
+        public void conectarConLaBaseDedatos()
+
+        {
+            conn.Open();
+
+        }
+
+        #endregion conectar con la base de datos
+
+
+        #region deconectar la base de datos
+
+        public void deconectarLaBaseDeDatos()
+        {
+
+            conn.Close();
+
+        }
+
+        #endregion deconectar la base datos
+
+    
+        #region actualizar Data Grid View
+
+        public void actualizarDataGridView(DataGridView dg, string consulta)
+
+        {
+
+            conectarConLaBaseDedatos();
+
+
+            System.Data.DataSet ds = new System.Data.DataSet();
+
+            System.Data.SqlClient.SqlDataAdapter da = new SqlDataAdapter(consulta, conn);
+
+            da.Fill(ds, "Clientes");
+
+            dg.DataSource = ds;
+            dg.DataMember = "Clientes";
+
+
+            deconectarLaBaseDeDatos();
+        }
+
+
+        #endregion actualizar Data Grid View
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Anderson Definition Gym System Final/forms/formularioPrincipal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anderson_Definition_Gym_System_Final
+{
+    public partial class formularioPrincipal : Form
+    {
+        public formularioPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        Conecxiones conecxiones = new Conecxiones();
+
+
+
+        private void formularioPrincipal_Load(object sender, EventArgs e)
+        {
+
+            conecxiones.actualizarDataGridView(dataGridView1, "select * from Clientes");
+            
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            conecxiones.actualizarDataGridView(dataGridView1, "select * from Clientes where nombre like '%"+ buscarNombre.Text +"%'");
+
+        }
+    }
+}
